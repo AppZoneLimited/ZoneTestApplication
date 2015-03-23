d3a6dfe7122f20831611d17eaae4911269e0ef6f
Akapo Damilola F <fdamilola@gmail.com>
2015-03-23 16:22:15 +0100

    Updated to handle in app files

--- src/com/example/testzone/FlowActivity.java
+++ src/com/example/testzone/FlowActivity.java
@@ -1,10 +1,21 @@
 package com.example.testzone;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
 import com.appzone.zone.orchestra.engine.MobileFlow;
+
 import com.appzone.zone.orchestra.engine.datatypes.Step;
 import com.appzone.zone.orchestra.engine.datatypes.StepsAbstraction;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -15,7 +26,7 @@ public class FlowActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_flow);
 
-		MobileFlow mf = new MobileFlow(this); // Create flow object using json
+		MobileFlow mf = new MobileFlow(loadJson(this)); // Create flow object using json
 												// from file. An alternative
 												// would be new MobileFlow(data)
 												// Where data is a string;
@@ -47,6 +58,45 @@ public class FlowActivity extends Activity {
 			e.printStackTrace();
 		}
 
+	}
+	
+	/*
+	 * This is a method for loading string from text file
+	 */
+	private String loadJson(Context ctx) {
+		String json = null;
+		InputStream is = ctx.getResources().openRawResource(R.raw.dejavujson);
+		Writer writer = new StringWriter();
+		char[] buffer = new char[1024];
+		Reader reader = null;
+		try {
+
+			try {
+				reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			int n;
+			try {
+				while ((n = reader.read(buffer)) != -1) {
+					writer.write(buffer, 0, n);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		json = writer.toString();
+		return json;
 	}
 
 }
